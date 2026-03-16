c120ac8358456b087a78b4b2c7912a270d7dd2d4
Gege <93001221+Geovana1711@users.noreply.github.com>
2026-03-15 21:46:40 -0300

    Create ex10.py

---
+++ ex10.py
@@ -0,0 +1,14 @@
+unidades = ["Zero", "Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", 
+            "Dez", "Onze", "Doze", "Treze", "Quatorze", "Quinze", "Dezesseis", "Dezessete", "Dezoito", "Dezenove"]
+dezenas = ["", "", "Vinte", "Trinta", "Quarenta", "Cinquenta", "Sessenta", "Setenta", "Oitenta", "Noventa"]
+num = int(input("Digite um número até 99: "))
+
+if num < 20:
+    print(unidades[num])
+else:
+    d = num // 10
+    u = num % 10
+    if u == 0:
+        print(dezenas[d])
+    else:
+        print(f"{dezenas[d]} e {unidades[u]}")
